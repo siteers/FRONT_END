8721687f44a5a0cac9eb3dd3e8a3676074286e22
Krzysztof Sitko <122014903+siteers@users.noreply.github.com>
2023-02-06 18:41:11 +0100

    Add files via upload

---
+++ probka.js
@@ -0,0 +1,40 @@
+
+var input = document.getElementById("input");
+var button = document.getElementById("button");
+var answer = document.getElementById("answer");
+var list = document.getElementById("list");
+
+var los = Math.floor(Math.random() * 10) + 1;
+
+button.addEventListener("click", sprawdz);
+
+function sprawdz()
+{
+    if (input.value > 0 && input.value <= 10){
+        if (input.value == los)
+        {   
+            var value = Number(input.value)
+            answer.innerHTML = value + " to prawidlowa cyfra brawo! ";
+        }
+        else
+        {   
+            var value = Number(input.value)
+            answer.innerHTML = value + " to nieprawidlowa cyfra!";
+            if (los > value)
+            {
+                answer.innerHTML = "Prawidlowa liczba jest<br> wieksza od " + value;
+            }
+            else
+            answer.innerHTML = "Prawidlowa liczba jest<br> mniejsza od " + value;
+        }
+    }
+    else
+    alert("liczba musi byc z przedzialu 1-10!");
+
+ 
+    var li = document.createElement("li")
+    list.appendChild(li);
+    list.lastChild.innerHTML = value;
+    
+}
+
